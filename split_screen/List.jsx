9e4fe6372bbe17de7b716e92871b9c40da74b1ab
msm <myatsumon@ucsy.edu.mm>
2023-12-18 21:46:37 +0630

    feat: modal

---
+++ split_screen/List.jsx
@@ -0,0 +1,45 @@
+// import React from "react";
+// import RegularList from "./components/list/Regular";
+// import { authors } from "./data/authors";
+// import { books } from "./data/books";
+// import LargeAuthorListItem from "./components/authors/LargeListItem";
+// import SmallAuthorListItem from "./components/authors/SmallListItem";
+// import SmallBookListItems from "./components/books/SmallListItem";
+// import LargeBookListItems from "./components/books/LargeListItem";
+// import NumberedList from "./components/list/Numbered";
+
+// const App = () => {
+//   return (
+//     <>
+//       <RegularList
+//         items={authors}
+//         sourceName={"author"}
+//         ItemComponent={SmallAuthorListItem}
+//       />
+//       <RegularList
+//         items={authors}
+//         sourceName={"author"}
+//         ItemComponent={LargeAuthorListItem}
+//       />
+
+//       <RegularList
+//         items={books}
+//         sourceName={"book"}
+//         ItemComponent={SmallBookListItems}
+//       />
+//       <RegularList
+//         items={books}
+//         sourceName={"book"}
+//         ItemComponent={LargeBookListItems}
+//       />
+
+//       <NumberedList
+//         items={books}
+//         sourceName={"book"}
+//         ItemComponent={LargeBookListItems}
+//       />
+//     </>
+//   );
+// };
+
+// export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
-import React from "react";
-import RegularList from "./components/list/Regular";
-import { authors } from "./data/authors";
-import { books } from "./data/books";
-import LargeAuthorListItem from "./components/authors/LargeListItem";
-import SmallAuthorListItem from "./components/authors/SmallListItem";
-import SmallBookListItems from "./components/books/SmallListItem";
-import LargeBookListItems from "./components/books/LargeListItem";
-import NumberedList from "./components/list/Numbered";
+import LargeBookListItems from './components/books/LargeListItem'
+import Modal from './components/Modal'
+import React from 'react'
+import { books } from './data/books'
 
 const App = () => {
   return (
-    <>
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={SmallAuthorListItem}
-      />
-      <RegularList
-        items={authors}
-        sourceName={"author"}
-        ItemComponent={LargeAuthorListItem}
-      />
+    <Modal><LargeBookListItems book={books[0]}/></Modal>
+  )
+}
 
-      <RegularList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={SmallBookListItems}
-      />
-      <RegularList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={LargeBookListItems}
-      />
-
-      <NumberedList
-        items={books}
-        sourceName={"book"}
-        ItemComponent={LargeBookListItems}
-      />
-    </>
-  );
-};
-
-export default App;
+export default App
